570c168555c3e25eefd9c2b3312170fb3e48c1c7
Andreas Prinz <andreas.pr81@gmail.com>
2026-05-20 20:39:47 +0300

    fix: restore missing parentheses in func main

--- 2-go-app/main.go
+++ 2-go-app/main.go
@@ -28,6 +28,7 @@ func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(`{"status":"ok"}`))
+}
 
 func main() {
 	// Ендпоінт для перевірки стану (Liveness/Readiness probes в K8s)
